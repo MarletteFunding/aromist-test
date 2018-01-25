0543608e1af84360899aec03862402174456af3a
Dheeraj Mummareddy <dheeraj.mummareddy@gmail.com>
2018-01-25 16:10:25 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/marlette/servicing/AromistTestApplication.java
@@ -0,0 +1,12 @@
+package com.marlette.servicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromistTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromistTestApplication.class, args);
+	}
+}
